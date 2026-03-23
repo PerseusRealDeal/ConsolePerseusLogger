00db181186933f003cf04edd4f0fec7a05cb7aae
Mikhail A.Z. <mzhigulin@gmail.com>
2026-03-24 00:35:36 +0700

    ADDED: minor changes

--- Tests/UnitTests/CPLStartTests.swift
+++ Tests/UnitTests/CPLStartTests.swift
@@ -27,13 +27,19 @@ final class FunctionalTests: XCTestCase {
         log.time = true
         log.owner = true
     }
-/*
+
     override static func tearDown() {
         super.tearDown()
+
+        log.message("[\(type(of: self))].\(#function)")
     }
 
+    /*
+
     func test_zero() { XCTFail("Tests not yet implemented in \(type(of: self)).") }
-*/
+
+     */
+
     func test_the_first_success() {
         log.message("[\(type(of: self))].\(#function)")
 
